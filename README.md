d3e60c4c73c1280272358ba65aabb87a84622bf3
Erik Belak <erik.belak@gmail.com>
2024-03-13 00:04:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Clicker
+
+Simple python clicker.
+
+## Usage
+
+1. Initiate clicking by clicking `t`
+2. Stop by clicking `t` again
+
+Test clicking [here](http://orteil.dashnet.org/experiments/cookie/)
+
+---
+Made with ❤️ and 🤪 (c) 2024
---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+import threading
+from pynput.mouse import Controller, Button
+from pynput.keyboard import Listener, KeyCode
+
+
+TOGGLE_KEY = KeyCode.from_char("t")
+
+clicking = False
+
+mouse = Controller()
+
+
+def clicker():
+    while True:
+        if clicking:
+            mouse.click(Button.left, 1)
+        time.sleep(0.5)
+
+
+def toggle_event(key):
+    if key == TOGGLE_KEY:
+        print('Pressed')
+        global clicking
+        clicking = not clicking
+
+
+click_thread = threading.Thread(target=clicker)
+click_thread.start()
+
+
+# Collect events until released
+with Listener(on_press=toggle_event) as listener:
+    print('Listening...')
+    try:
+        listener.join()
+        print('Listening...')
+    except Exception as e:
+        print('{0} was pressed'.format(e.args[0]))
